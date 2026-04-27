f0fa92912bf72c2bf2edc6f73f8ac907195c36ea
RAPHAEL DE LEMOS PIRES <raphaellpires@gmail.com>
2026-04-27 08:07:25 -0300

    Placing pieces on the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -25,7 +25,10 @@ public class Board {
         return pieces[position.getRow()][position.getCol()];
     }
 
-
+    public void placePiece(Piece piece, Position position){
+        pieces[position.getRow()][position.getCol()] = piece;
+        piece.position = position;
+    }
 
 
 }
